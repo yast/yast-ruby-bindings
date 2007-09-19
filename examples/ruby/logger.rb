234f91cc9e70389a0efcf89a784e203deeb7e6f4
Duncan Mac-Vicar <dmacvicar@suse.de>
2007-09-19 15:50:49 +0000

    reorganize examples
    svn path=/trunk/ruby-bindings/; revision=40968

---
+++ examples/ruby/logger.rb
@@ -0,0 +1,5 @@
+require 'yast'
+include YaST
+#ui = YaST::Module.new("UI")
+
+y2milestone("Hello")
---
+++ examples/ruby/ui.rb
@@ -0,0 +1,22 @@
+require 'yast'
+ui = YaST::Module.new("UI")
+
+include YaST::Ui
+
+t = HBox( Label("Welcome to Ruby!"), PushButton("Push me") )
+
+puts "#{t.to_s} #{t.class}"
+
+ui.OpenDialog(t)
+ui.UserInput()
+
+# You can also use downcase, as the symbols are aliased
+t = hbox( label("Welcome to Ruby!"), pushbutton("Push me") )
+
+puts "#{t.to_s} #{t.class}"
+
+ui.OpenDialog(t)
+ui.UserInput()
+
+
+exit
