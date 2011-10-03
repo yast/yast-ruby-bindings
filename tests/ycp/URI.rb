9d9e0f452520b9b8ccc4a8b4ad8d031a4db99e40
Martin Vidner <mvidner@suse.cz>
2011-10-03 15:23:35 +0000

    Force garbage collection to test memory allocation bugs.
    svn path=/trunk/ruby-bindings/; revision=66204

--- tests/ycp/URI.rb
+++ tests/ycp/URI.rb
@@ -1,11 +1,13 @@
 require 'uri'
 module URI
-  # URI::parse works even without glue?
-  #def self.parse(uri_string)
-    
-  #end
+  # URI::parse works even without glue
   
   def self.scheme(instance)
     instance.scheme
+  end
+
+  # garbage_collect
+  class << self;
+    include GC
   end
 end
