b8802467448eeddd93bad731b857c4c488542137
Ladislav Slezák <lslezak@suse.cz>
2013-01-22 13:10:13 +0100

    added YCP::Term

---
+++ src/ruby/ycp/term.rb
@@ -0,0 +1,26 @@
+module YCP
+  class Term
+    # term symbol
+    attr_reader :value
+    # term parameters
+    attr_reader :params
+
+    def initialize value, *params
+      @value = value
+      @params = *params
+    end
+
+    def == second
+      value == second.value && params == second.params
+    end
+
+    def != second
+      !(self == second)
+    end
+    
+    def [] index
+      params[index]
+    end
+    
+  end
+end
---
+++ tests/ruby/term_test.rb
@@ -0,0 +1,28 @@
+$LOAD_PATH << File.dirname(__FILE__)
+require "test_helper"
+
+require "ycp/term"
+
+class TermTest < YCP::TestCase
+  def test_initialize
+    assert_equal :HBox, YCP::Term.new(:HBox).value
+    assert_equal [], YCP::Term.new(:HBox).params
+    assert_equal ["test"], YCP::Term.new(:HBox, "test").params
+    assert_equal "test", YCP::Term.new(:HBox, "test").params.first
+    
+    assert_equal :VBox, YCP::Term.new(:HBox, YCP::Term.new(:VBox)).params.first.value
+  end
+  
+  def test_update
+    t = YCP::Term.new(:HBox, 1, 2)
+    t.params[0] = 0
+    assert_equal t.params.first, 0
+  end
+
+  def test_equal
+    assert_equal YCP::Term.new(:HBox), YCP::Term.new(:HBox)
+    assert_not_equal YCP::Term.new(:HBox), YCP::Term.new(:VBox)
+    assert_not_equal YCP::Term.new(:HBox), YCP::Term.new(:HBox, "test")
+  end
+
+end
