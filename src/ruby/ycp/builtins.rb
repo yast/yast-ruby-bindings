be622baf36fb20b86c1e2bda60ac93213ae357a3
Ladislav Slezák <lslezak@suse.cz>
2013-01-24 12:52:55 +0100

    added all YCP builtin definitions

--- src/ruby/ycp/builtins.rb
+++ src/ruby/ycp/builtins.rb
@@ -10,7 +10,7 @@ module YCP
       case object
       when Array then return object + params
       when Hash then  return object.merge(Hash[*params])
-      #TODO when YCP::Term:
+        #TODO when YCP::Term:
       when YCP::Path then return object + params.first
       else
         raise "Invalid object for add builtin"
@@ -166,6 +166,625 @@ module YCP
     def self.toset array
       return nil if array.nil?
       array.uniq.sort
+    end
+
+    ###########################################################
+    # YCP Byteblock Builtins
+    ###########################################################
+
+    # Returns a size of a byteblock in bytes.
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Converts a value to a byteblock.
+    def self.tobyteblock
+      raise "Builtin tobyteblock() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP Float Builtins
+    ###########################################################
+
+    module Float
+    	# absolute value
+      def self.abs
+        raise "Builtin abs() is not implemented yet"
+      end
+
+    	# round upwards to integer
+      def self.ceil
+        raise "Builtin ceil() is not implemented yet"
+      end
+
+    	# round downwards to integer
+      def self.floor
+        raise "Builtin floor() is not implemented yet"
+      end
+
+    	# power function
+      def self.pow
+        raise "Builtin pow() is not implemented yet"
+      end
+
+    	# Converts a floating point number to a localized string
+      def self.tolstring
+        raise "Builtin tolstring() is not implemented yet"
+      end
+
+    	# round to integer, towards zero
+      def self.trunc
+        raise "Builtin trunc() is not implemented yet"
+      end
+    end
+    
+    # Converts a value to a floating point number.
+    def self.tofloat
+      raise "Builtin tofloat() is not implemented yet"
+    end
+
+    # Converts a floating point number to a string
+    def self.tostring
+      raise "Builtin tostring() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP Integer Builtins
+    ###########################################################
+
+    # Converts a value to an integer.
+    def self.tointeger
+      raise "Builtin tointeger() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP List Builtins
+    ###########################################################
+
+    # Create a new list with a new element
+    def self.add
+      raise "Builtin add() is not implemented yet"
+    end
+
+    # Changes a list. Deprecated, use LIST[size(LIST)] = value.
+    def self.change
+      raise "Builtin change() is not implemented yet"
+    end
+
+    # Checks if a list contains an element
+    def self.contains
+      raise "Builtin contains() is not implemented yet"
+    end
+
+    # Filters a List
+    def self.filter
+      raise "Builtin filter() is not implemented yet"
+    end
+
+    # Searches for the first occurence of a certain element in a list
+    def self.find
+      raise "Builtin find() is not implemented yet"
+    end
+
+    # Flattens List
+    def self.flatten
+      raise "Builtin flatten() is not implemented yet"
+    end
+    
+    # Processes the content of a list
+    def self.foreach
+      raise "Builtin foreach() is not implemented yet"
+    end
+
+    # Returns whether the list l is empty.
+    def self.isempty
+      raise "Builtin isempty() is not implemented yet"
+    end
+
+    module List
+      # Reduces a list to a single value.
+      def self.reduce
+        raise "Builtin list::reduce() is not implemented yet"
+      end
+
+      # Reduces a list to a single value.
+      def self.reduce
+        raise "Builtin list::reduce() is not implemented yet"
+      end
+
+      # Creates new list with swaped elemetns at offset i1 and i2.
+      def self.swap
+        raise "Builtin list::swap() is not implemented yet"
+      end
+    end
+
+    # Maps an operation onto all elements of a list and thus creates a map.
+    def self.listmap
+      raise "Builtin listmap() is not implemented yet"
+    end
+
+    # Sort A List respecting locale
+    def self.lsort
+      raise "Builtin lsort() is not implemented yet"
+    end
+
+    # Maps an operation onto all elements of a list and thus creates a new list.
+    def self.maplist
+      raise "Builtin maplist() is not implemented yet"
+    end
+
+    # Merges two lists into one
+    def self.merge
+      raise "Builtin merge() is not implemented yet"
+    end
+
+    # Prepends a list with a new element
+    def self.prepend
+      raise "Builtin prepend() is not implemented yet"
+    end
+
+    # Removes element from a list
+    def self.remove
+      raise "Builtin remove() is not implemented yet"
+    end
+
+    # Selects a list element (deprecated, use LIST[INDEX]:DEFAULT)
+    def self.select
+      raise "Builtin select() is not implemented yet"
+    end
+
+    # Checks if a sorted list contains an element
+    def self.setcontains
+      raise "Builtin setcontains() is not implemented yet"
+    end
+
+    # Returns size of list
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Sorts a List according to the YCP builtin predicate
+    def self.sort
+      raise "Builtin sort() is not implemented yet"
+    end
+
+    # Sort list using an expression
+    def self.sort
+      raise "Builtin sort() is not implemented yet"
+    end
+
+    # Split a string by delimiter
+    def self.splitstring
+      raise "Builtin splitstring() is not implemented yet"
+    end
+
+    # Extracts a sublist
+    def self.sublist
+      raise "Builtin sublist() is not implemented yet"
+    end
+
+    # Extracts a sublist
+    def self.sublist
+      raise "Builtin sublist() is not implemented yet"
+    end
+
+    # Converts a value to a list (deprecated, use (list)VAR).
+    def self.tolist
+      raise "Builtin tolist() is not implemented yet"
+    end
+
+    # Sorts list and removes duplicates
+    def self.toset
+      raise "Builtin toset() is not implemented yet"
+    end
+
+    # Unions of lists
+    def self.union
+      raise "Builtin union() is not implemented yet"
+    end
+
+    ###########################################################
+    # Map Builtins
+    ###########################################################
+
+    # Add a key/value pair to a map
+    def self.add
+      raise "Builtin add() is not implemented yet"
+    end
+
+    # Change element pair in a map. Deprecated, use MAP[KEY] = VALUE.
+    def self.change
+      raise "Builtin change() is not implemented yet"
+    end
+
+    # Filter a Map
+    def self.filter
+      raise "Builtin filter() is not implemented yet"
+    end
+
+    # Process the content of a map
+    def self.foreach
+      raise "Builtin foreach() is not implemented yet"
+    end
+
+    # Check if map has a certain key
+    def self.haskey
+      raise "Builtin haskey() is not implemented yet"
+    end
+
+    # Returns whether the map m is empty.
+    def self.isempty
+      raise "Builtin isempty() is not implemented yet"
+    end
+
+    # Select a map element (deprecated, use MAP[KEY]:DEFAULT)
+    def self.lookup
+      raise "Builtin lookup() is not implemented yet"
+    end
+
+    # Maps an operation onto all elements key/value and create a list
+    def self.maplist
+      raise "Builtin maplist() is not implemented yet"
+    end
+
+    # Maps an operation onto all key/value pairs of a map
+    def self.mapmap
+      raise "Builtin mapmap() is not implemented yet"
+    end
+
+    # Remove key/value pair from a map
+    def self.remove
+      raise "Builtin remove() is not implemented yet"
+    end
+
+    # Size of a map
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Converts a value to a map.
+    def self.tomap
+      raise "Builtin tomap() is not implemented yet"
+    end
+
+    # Union of 2 maps
+    def self.union
+      raise "Builtin union() is not implemented yet"
+    end
+
+    ###########################################################
+    # Miscellaneous YCP Builtins
+    ###########################################################
+
+    # Evaluate a YCP value.
+    def self.eval
+      raise "Builtin eval() is not implemented yet"
+    end
+
+    # Change or add an environment variable
+    def self.getenv
+      raise "Builtin getenv() is not implemented yet"
+    end
+
+    # Checks whether a value is of a certain type
+    def self.is
+      raise "Builtin is() is not implemented yet"
+    end
+
+    # Random number generator.
+    def self.random
+      raise "Builtin random() is not implemented yet"
+    end
+
+    # Change or add an environment variable
+    def self.setenv
+      raise "Builtin setenv() is not implemented yet"
+    end
+    # Change or add an environment variable
+    def self.setenv
+      raise "Builtin setenv() is not implemented yet"
+    end
+
+    # Format a String
+    def self.sformat
+      raise "Builtin sformat() is not implemented yet"
+    end
+
+    # Sleeps a number of milliseconds.
+    def self.sleep
+      raise "Builtin sleep() is not implemented yet"
+    end
+
+    # Initialize random number generator
+    def self.srandom
+      raise "Builtin srandom() is not implemented yet"
+    end
+
+    # Initialize random number generator.
+    def self.srandom
+      raise "Builtin srandom() is not implemented yet"
+    end
+
+    # Return the number of seconds since 1.1.1970.
+    def self.time
+      raise "Builtin time() is not implemented yet"
+    end
+
+    # Log a message to the y2log.
+    def self.y2debug
+      raise "Builtin y2debug() is not implemented yet"
+    end
+
+    # Log an error to the y2log.
+    def self.y2error
+      raise "Builtin y2error() is not implemented yet"
+    end
+
+    # Log an internal message to the y2log.
+    def self.y2internal
+      raise "Builtin y2internal() is not implemented yet"
+    end
+
+    # Log a milestone to the y2log.
+    def self.y2milestone
+      raise "Builtin y2milestone() is not implemented yet"
+    end
+
+    # Log a security message to the y2log.
+    def self.y2security
+      raise "Builtin y2security() is not implemented yet"
+    end
+
+    # Log an user-level system message to the y2changes
+    def self.y2useritem
+      raise "Builtin y2useritem() is not implemented yet"
+    end
+
+    # Log an user-level addional message to the y2changes
+    def self.y2usernote
+      raise "Builtin y2usernote() is not implemented yet"
+    end
+
+    # Log a warning to the y2log.
+    def self.y2warning
+      raise "Builtin y2warning() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP Path Builtins
+    ###########################################################
+
+    # Add a path element to existing path
+    def self.add
+      raise "Builtin add() is not implemented yet"
+    end
+
+    # Returns the number of path elements
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Converts a value to a path.
+    def self.topath
+      raise "Builtin topath() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP String Builtins
+    ###########################################################
+
+    # Encrypts a string
+    def self.crypt
+      raise "Builtin crypt() is not implemented yet"
+    end
+
+    # Encrypts a string using bigcrypt
+    def self.cryptbigcrypt
+      raise "Builtin cryptbigcrypt() is not implemented yet"
+    end
+
+    # Encrypts a string with blowfish
+    def self.cryptblowfish
+      raise "Builtin cryptblowfish() is not implemented yet"
+    end
+
+    # Encrypts a string using md5
+    def self.cryptmd5
+      raise "Builtin cryptmd5() is not implemented yet"
+    end
+
+    # Removes all characters from a string
+    def self.deletechars
+      raise "Builtin deletechars() is not implemented yet"
+    end
+
+    # Translates the text using the given text domain
+    def self.dgettext
+      raise "Builtin dgettext() is not implemented yet"
+    end
+
+    # Translates the text using a locale-aware plural form handling
+    def self.dngettext
+      raise "Builtin dngettext() is not implemented yet"
+    end
+
+    # Translates the text using the given text domain and path
+    def self.dpgettext
+      raise "Builtin dpgettext() is not implemented yet"
+    end
+
+    # Filters characters out of a String
+    def self.filterchars
+      raise "Builtin filterchars() is not implemented yet"
+    end
+
+    # Returns position of a substring
+    def self.find
+      raise "Builtin find() is not implemented yet"
+    end
+
+    # Searches string for the first non matching chars
+    def self.findfirstnotof
+      raise "Builtin findfirstnotof() is not implemented yet"
+    end
+
+    # Finds position of the first matching characters in string
+    def self.findfirstof
+      raise "Builtin findfirstof() is not implemented yet"
+    end
+
+    # Searches the last element of string that doesn't match
+    def self.findlastnotof
+      raise "Builtin findlastnotof() is not implemented yet"
+    end
+
+    # Searches string for the last match
+    def self.findlastof
+      raise "Builtin findlastof() is not implemented yet"
+    end
+
+    # Returns whether the string s is empty.
+    def self.isempty
+      raise "Builtin isempty() is not implemented yet"
+    end
+
+    # searches for a specific string within another string
+    def self.issubstring
+      raise "Builtin issubstring() is not implemented yet"
+    end
+
+    # Extracts a substring in UTF-8 encoded string
+    def self.lsubstring
+      raise "Builtin lsubstring() is not implemented yet"
+    end
+
+    # Extracts a substring in UTF-8 encoded string
+    def self.lsubstring
+      raise "Builtin lsubstring() is not implemented yet"
+    end
+
+    # Joins list elements with a string
+    def self.mergestring
+      raise "Builtin mergestring() is not implemented yet"
+    end
+
+    # Searches a string for a POSIX Extended Regular Expression match.
+    def self.regexpmatch
+      raise "Builtin regexpmatch() is not implemented yet"
+    end
+
+    # Returns a pair with position and length of the first match.
+    def self.regexppos
+      raise "Builtin regexppos() is not implemented yet"
+    end
+
+    # Regex Substitution
+    def self.regexpsub
+      raise "Builtin regexpsub() is not implemented yet"
+    end
+
+    # Regex tokenize
+    def self.regexptokenize
+      raise "Builtin regexptokenize() is not implemented yet"
+    end
+
+    # Returns position of a substring
+    def self.search
+      raise "Builtin search() is not implemented yet"
+    end
+
+    # Returns the number of characters of the string s
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Returns part of a string
+    def self.substring
+      raise "Builtin substring() is not implemented yet"
+    end
+
+    # Extracts a substring
+    def self.substring
+      raise "Builtin substring() is not implemented yet"
+    end
+
+    # Returns time string
+    def self.timestring
+      raise "Builtin timestring() is not implemented yet"
+    end
+
+    # Returns characters below 0x7F included in STRING
+    def self.toascii
+      raise "Builtin toascii() is not implemented yet"
+    end
+
+    # Converts an integer to a hexadecimal string.
+    def self.tohexstring
+      raise "Builtin tohexstring() is not implemented yet"
+    end
+
+    # Converts an integer to a hexadecimal string.
+    def self.tohexstring
+      raise "Builtin tohexstring() is not implemented yet"
+    end
+
+    # Makes a string lowercase
+    def self.tolower
+      raise "Builtin tolower() is not implemented yet"
+    end
+
+    # Converts a value to a string.
+    def self.tostring
+      raise "Builtin tostring() is not implemented yet"
+    end
+
+    # Makes a string uppercase
+    def self.toupper
+      raise "Builtin toupper() is not implemented yet"
+    end
+
+    ###########################################################
+    # YCP Term Builtins
+    ###########################################################
+
+    # Add value to term
+    def self.add
+      raise "Builtin add() is not implemented yet"
+    end
+
+    # Returns the arguments of a term.
+    def self.argsof
+      raise "Builtin argsof() is not implemented yet"
+    end
+
+    # Remove item from term
+    def self.remove
+      raise "Builtin remove() is not implemented yet"
+    end
+
+    # Select item from term
+    def self.select
+      raise "Builtin select() is not implemented yet"
+    end
+
+    # Returns the number of arguments of the term TERM.
+    def self.size
+      raise "Builtin size() is not implemented yet"
+    end
+
+    # Returns the symbol of the term TERM.
+    def self.symbolof
+      raise "Builtin symbolof() is not implemented yet"
+    end
+
+    # Converts a value to a term.
+    def self.toterm
+      raise "Builtin toterm() is not implemented yet"
+    end
+
+    # Constructs a term from a symbol and a list.
+    def self.toterm
+      raise "Builtin toterm() is not implemented yet"
     end
 
   end
