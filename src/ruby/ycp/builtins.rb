b324bc7f4881573b491e00fd703f6e96719a2a29
Ladislav Slezák <lslezak@suse.cz>
2013-01-24 13:25:36 +0100

    resolved overloaded add() builtin

--- src/ruby/ycp/builtins.rb
+++ src/ruby/ycp/builtins.rb
@@ -5,6 +5,28 @@ module YCP
   module Builtins
 
     ###########################################################
+    # Overloaded Builtins
+    ###########################################################
+
+    # Add a key/value pair to a map - add(<map>, <key>, <value)
+    # Create a new list with a new element - add(<list>, <value>)
+    # Add value to term - add(<term>, <value>)
+    # Add a path element to existing path - add(<path>, <value>)
+    # Method that simulates behavior of add in ycp builtin.
+    # Most notably difference is that it always create new object
+    # For new code it is recommended to use directly methods on objects
+    def self.add object, *params
+      case object
+      when Array then return object + params
+      when Hash then  return object.merge(Hash[*params])
+        #TODO when YCP::Term:
+      when YCP::Path then return object + params.first
+      else
+        raise "Invalid object for add builtin"
+      end
+    end
+
+    ###########################################################
     # YCP Byteblock Builtins
     ###########################################################
 
@@ -76,11 +98,6 @@ module YCP
     ###########################################################
     # YCP List Builtins
     ###########################################################
-
-    # Create a new list with a new element
-    def self.add
-      raise "Builtin add() is not implemented yet"
-    end
 
     # Changes a list. Deprecated, use LIST[size(LIST)] = value.
     def self.change
@@ -241,21 +258,6 @@ module YCP
     ###########################################################
     # Map Builtins
     ###########################################################
-
-    # Add a key/value pair to a map
-    # Method that simulates behavior of add in ycp builtin.
-    # Most notably difference is that it always create new object
-    # For new code it is recommended to use directly methods on objects
-    def self.add object, *params
-      case object
-      when Array then return object + params
-      when Hash then  return object.merge(Hash[*params])
-        #TODO when YCP::Term:
-      when YCP::Path then return object + params.first
-      else
-        raise "Invalid object for add builtin"
-      end
-    end
 
     # Change element pair in a map. Deprecated, use MAP[KEY] = VALUE.
     def self.change
@@ -419,11 +421,6 @@ module YCP
     ###########################################################
     # YCP Path Builtins
     ###########################################################
-
-    # Add a path element to existing path
-    def self.add
-      raise "Builtin add() is not implemented yet"
-    end
 
     # Returns the number of path elements
     def self.size
@@ -670,11 +667,6 @@ module YCP
     ###########################################################
     # YCP Term Builtins
     ###########################################################
-
-    # Add value to term
-    def self.add
-      raise "Builtin add() is not implemented yet"
-    end
 
     # Returns the arguments of a term.
     def self.argsof
