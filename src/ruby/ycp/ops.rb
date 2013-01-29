0c631ae019717215154099b706b7ca800425ecdb
Josef Reidinger <jreidinger@suse.cz>
2013-01-29 18:19:25 +0000

    add comparison operation

--- src/ruby/ycp/ops.rb
+++ src/ruby/ycp/ops.rb
@@ -6,4 +6,103 @@ module Ops
   def self.not_equal first, second
     return !equal(first, second)
   end
+
+  def self.less_than first, second
+    return nil if first.nil? || second.nil?
+
+    first = comparable_object(first)
+
+    return first < second
+  end
+
+  def self.less_or_equal first, second
+    return nil if first.nil? || second.nil?
+
+    first = comparable_object(first)
+
+    return first <= second
+  end
+
+  def self.greater_than first, second
+    return nil if first.nil? || second.nil?
+
+    first = comparable_object(first)
+
+    return first > second
+  end
+
+  def self.greater_or_equal first, second
+    return nil if first.nil? || second.nil?
+
+    first = comparable_object(first)
+
+    return first >= second
+  end
+
+  def self.comparable_object object
+    # we have guarantie that we get same types 
+    # so we create object that can compare same type
+    case object
+    when TrueClass, FalseClass
+      return BooleanComparator.new object
+    when NilClass
+      return NilComparable.new
+    when Array
+      return ListComparator.new object
+    when Hash
+      #hash is really bogus piece, as it compare on quite undefined key hash value
+      raise "Not yet supported"
+    else
+      return object
+    end
+  end
+
+  class NilComparable
+    def <=>(second)
+      return 0 if second.nil?
+      return -1
+    end
+  end
+
+  class BooleanComparator
+    include Comparable
+    def initialize value
+      @value = value
+    end
+
+    def <=>(second)
+      if @value == second
+        return 0
+      end
+
+      #rule is that true always rules over false
+      return @value ? 1 : -1
+    end
+  end
+
+  class ListComparator
+    include Comparable
+    def initialize value
+      @value = value
+    end
+
+    def <=>(second)
+      min_size = [@value.size,second.size].min
+      0.upto(min_size-1) do |i|
+        #stupid nil handling
+        fval = @value[i]
+        sval = second[i]
+        if (sval.nil? && !fval.nil? )
+          return 1
+        end
+
+        # we need to use out builtin, but also we need to 
+        res = Ops.comparable_object(fval) <=> sval
+        return res if res != 0
+      end
+      # no decision yet
+      return @value.size <=> second.size
+    end
+  end
+
 end
--- tests/ruby/ops_test.rb
+++ tests/ruby/ops_test.rb
@@ -63,4 +63,81 @@ class OpsTest < YCP::TestCase
       assert !Ops.equal(nil,second), "Nil should differ from value, but marked as same \n+#{second.inspect}"
     end
   end
+
+  def test_comparison_int
+    assert_equal true, Ops.less_than(1,2)
+    assert_equal true, Ops.less_or_equal(1,1)
+    assert_equal true, Ops.greater_than(3,2)
+    assert_equal true, Ops.greater_or_equal(2,2)
+    assert_equal false, Ops.less_than(2,2)
+    assert_equal false, Ops.less_or_equal(2,1)
+    assert_equal false, Ops.greater_than(2,2)
+    assert_equal false, Ops.greater_or_equal(2,3)
+  end
+
+  def test_comparison_float
+    assert_equal true, Ops.less_than(1.0,1.1)
+    assert_equal true, Ops.less_or_equal(1.0,1.0)
+    assert_equal true, Ops.greater_than(2.1,2.0)
+    assert_equal true, Ops.greater_or_equal(2.0,2.0)
+    assert_equal false, Ops.less_than(2.0,2.0)
+    assert_equal false, Ops.less_or_equal(1.1,1.0)
+    assert_equal false, Ops.greater_than(2.0,2.0)
+    assert_equal false, Ops.greater_or_equal(2.0,2.1)
+  end
+
+  def test_comparison_string
+    assert_equal true, Ops.less_than("s","sa")
+    assert_equal true, Ops.less_or_equal("s","s")
+    assert_equal true, Ops.greater_than("ta","t")
+    assert_equal true, Ops.greater_or_equal("t","t")
+    assert_equal false, Ops.less_than("t","t")
+    assert_equal false, Ops.less_or_equal("sa","s")
+    assert_equal false, Ops.greater_than("t","t")
+    assert_equal false, Ops.greater_or_equal("t","ta")
+  end
+
+  def test_comparison_symbols
+    assert_equal true, Ops.less_than(:s,:sa)
+    assert_equal true, Ops.less_or_equal(:s,:s)
+    assert_equal true, Ops.greater_than(:ta,:t)
+    assert_equal true, Ops.greater_or_equal(:t,:t)
+    assert_equal false, Ops.less_than(:t,:t)
+    assert_equal false, Ops.less_or_equal(:sa,:s)
+    assert_equal false, Ops.greater_than(:t,:t)
+    assert_equal false, Ops.greater_or_equal(:t,:ta)
+  end
+
+  def test_comparison_booleans
+    assert_equal true, Ops.less_than(false,true)
+    assert_equal true, Ops.less_or_equal(false,false)
+    assert_equal true, Ops.greater_than(true,false)
+    assert_equal true, Ops.greater_or_equal(false,false)
+    assert_equal false, Ops.less_than(false,false)
+    assert_equal false, Ops.less_or_equal(true,false)
+    assert_equal false, Ops.greater_than(false,false)
+    assert_equal false, Ops.greater_or_equal(false,true)
+  end
+
+  def test_comparison_list
+    assert_equal true, Ops.less_than([1],[1,2])
+    assert_equal true, Ops.less_than([1,1],[2])
+    assert_equal true, Ops.less_than([nil,nil,5],[nil,2])
+    assert_equal true, Ops.less_or_equal([1,1],[2])
+    assert_equal false, Ops.less_than([1,2],[1])
+    assert_equal false, Ops.less_than([2],[1,1])
+    assert_equal false, Ops.less_than([nil,5],[nil,nil,2])
+    assert_equal false, Ops.less_or_equal([2],[1,1])
+  end
+
+  def test_comparison_nil
+    assert_equal nil, Ops.less_than(1,nil)
+    assert_equal nil, Ops.less_or_equal(1,nil)
+    assert_equal nil, Ops.greater_than(3,nil)
+    assert_equal nil, Ops.greater_or_equal(2,nil)
+    assert_equal nil, Ops.less_than(nil,2)
+    assert_equal nil, Ops.less_or_equal(nil,1)
+    assert_equal nil, Ops.greater_than(nil,2)
+    assert_equal nil, Ops.greater_or_equal(nil,3)
+  end
 end
