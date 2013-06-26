784b05bb1054fd663de8d96b11461dcb5b5f5b1b
Ladislav Slezak <lslezak@novell.com>
2013-06-26 09:43:39 +0200

    support backtrace in y2log()
    triggered via -1 as the first parameter

--- src/ruby/yast/logger.rb
+++ src/ruby/yast/logger.rb
@@ -5,14 +5,28 @@ module Yast
   module_function
   def y2_logger_helper(level,args)
     caller_frame = 1
+    backtrace = false
+
     if args.first.is_a? Fixnum
-      caller_frame = caller_frame + args.shift
+      if args.first < 0
+        backtrace = true
+        args.shift
+        caller_frame = 2
+      else
+        caller_frame = caller_frame + args.shift
+      end
     end
 
     res = Builtins.sformat(*args)
     res.gsub!(/%/,"%%") #reescape all %
     caller[caller_frame] =~ /(.+):(\d+):in `([^']+)'/
     y2_logger(level, "Ruby", $1, $2.to_i, "", res)
+
+    if backtrace
+      y2_logger_helper(level, [2, "------------- Backtrace begin -------------"])
+      caller(3).each {|frame| y2_logger_helper(level, [4, frame])}
+      y2_logger_helper(level, [2, "------------- Backtrace end ---------------"])
+    end
   end
 
   module_function
