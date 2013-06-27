960961d4ea55c5104450ddf339fe10db0ef98782
Josef Reidinger <jreidinger@suse.cz>
2013-06-27 20:59:16 +0200

    support single index for assign

--- src/ruby/yast/ops.rb
+++ src/ruby/yast/ops.rb
@@ -64,8 +64,11 @@ module Yast
 
     def self.assign (object, indexes, value)
       return if indexes.nil? || object.nil?
+
       last = indexes.pop
       res = object
+      indexes = [indexes] unless indexes.is_a? ::Array
+
       indexes.each do |i|
         case res
         when ::Array, Yast::Term
