77c37869d927b14b3b7702a3599fe47c33a7ef3d
Josef Reidinger <jreidinger@suse.cz>
2013-09-12 09:51:47 +0200

    fix loading rubygems in embedded ruby

--- src/binary/YRuby.cc
+++ src/binary/YRuby.cc
@@ -65,6 +65,14 @@ YRuby::YRuby()
   ruby_init();
   ruby_init_loadpath();
 
+  // FIX for setup gem load path. Embedded ruby initialization mixes up gem 
+  // initialization (which we want) with option processing (which we don't want).
+  // Copying only needed parts of `ruby_options` here.
+  // See http://subforge.org/blogs/show/1
+  // Note that the solution is different to not touch internal ruby
+  rb_define_module("Gem");
+  rb_require("rubygems");
+
   rb_enc_find_index("encdb");
 
   VALUE ycp_references = Data_Wrap_Struct(rb_cObject, gc_mark, gc_free, & value_references_from_ycp);
