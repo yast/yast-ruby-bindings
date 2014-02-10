49b1b293a0cf70c6a4bab741523e0f2e70f4a092
Josef Reidinger <jreidinger@suse.cz>
2014-02-10 18:48:04 +0100

    fix tests

--- tests/ruby/test_module/clients/test_client.rb
+++ tests/ruby/test_module/clients/test_client.rb
@@ -2,9 +2,11 @@ module Yast
   class TestClient
     def main
       Yast.include "example.rb"
+      test_plus_five
       @test
+      5
     end
   end
 end
 
-Yast.TestClient.new
+Yast.TestClient.new.main
--- tests/ruby/wfm_spec.rb
+++ tests/ruby/wfm_spec.rb
@@ -8,12 +8,12 @@ module Yast
   describe WFM do
     describe ".CallFunction" do
       it "calls yast client via component system returning its value" do
-        expect(WFM.CallFunction("test_client")).to == 15
+        expect(WFM.CallFunction("test_client")).to eq 15
       end
 
       it "always properly initialize client (BNC#861529)" do
-        expect(WFM.CallFunction("test_client")).to == 15
-        expect(WFM.CallFunction("test_client")).to == 15
+        expect(WFM.CallFunction("test_client")).to eq 15
+        expect(WFM.CallFunction("test_client")).to eq 15
       end
     end
   end
