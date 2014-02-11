7462d0bfd6e6f481eb3e7288a932fe2afe110da0
Josef Reidinger <jreidinger@suse.cz>
2014-02-11 15:35:57 +0100

    try to fix client test

--- tests/ruby/test_helper.rb
+++ tests/ruby/test_helper.rb
@@ -7,5 +7,5 @@ end
 $:.unshift binary_path # yastx.so
 $:.unshift "#{ROOT_DIR}/src/ruby"       # yast.rb
 ENV["LD_LIBRARY_PATH"] = binary_path
-ENV["Y2DIR"] = File.dirname(__FILE__) + "/test_module"
+ENV["Y2DIR"] = (ENV["Y2DIR"] ? ENV["Y2DIR"] + ":" : "") + File.dirname(__FILE__) + "/test_module"
 
