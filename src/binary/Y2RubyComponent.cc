1e4b005ffe4fea8497f4fb355445127a9e4e8bcd
Josef Reidinger <jreidinger@suse.cz>
2014-04-22 11:31:07 +0200

    locale independent character conversion in module path (bnc#852242)

--- src/binary/Y2RubyComponent.cc
+++ src/binary/Y2RubyComponent.cc
@@ -94,6 +94,9 @@ const string Y2RubyComponent::CamelCase2DelimSepated( const char* name)
   size_t size = res.size();
   if (size==0)
     return res;
+  // convert always in C locale (bnc#852242)
+  char *old_locale = setlocale(LC_ALL, NULL);
+  setlocale(LC_ALL, "C");
   res[0] = tolower(res[0]);
   //first character and first char after :: is lowercase without underscore
   for(size_t i = res.find("::"); i!= string::npos; i = res.find("::",i+1))
@@ -111,5 +114,6 @@ const string Y2RubyComponent::CamelCase2DelimSepated( const char* name)
       res.replace(i,1,tmp); //replace upper by _lower
     }
   }
+  setlocale(LC_ALL, old_locale);
   return res;
 }
