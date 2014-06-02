30dd1706a34ceeb91f19a8270887b46d981699fb
Martin Vidner <mvidner@suse.cz>
2014-06-02 16:21:25 +0200

    Convert specs to RSpec 2.14.7 syntax with Transpec
    This conversion is done by Transpec 2.2.0 with the following command:
        transpec tests/ruby
    
    * 6 conversions
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)
    
    See also: https://github.com/yujinakayama/transpec#supported-conversions

--- tests/ruby/y2logger_spec.rb
+++ tests/ruby/y2logger_spec.rb
@@ -14,32 +14,32 @@ module Yast
     end
 
     it "logs debug messages via y2debug()" do
-      Yast.should_receive(:y2debug).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2debug).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.debug TEST_MESSAGE
     end
 
     it "logs info messages via y2milestone()" do
-      Yast.should_receive(:y2milestone).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2milestone).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.info TEST_MESSAGE
     end
 
     it "logs warnings via y2warning()" do
-      Yast.should_receive(:y2warning).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2warning).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.warn TEST_MESSAGE
     end
 
     it "logs errors via y2error()" do
-      Yast.should_receive(:y2error).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2error).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.error TEST_MESSAGE
     end
 
     it "logs fatal errors via y2error()" do
-      Yast.should_receive(:y2error).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2error).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.fatal TEST_MESSAGE
     end
 
     it "handles a message passed via block" do
-      Yast.should_receive(:y2milestone).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
+      expect(Yast).to receive(:y2milestone).with(Y2Logger::CALL_FRAME, TEST_MESSAGE)
       @test_logger.info { TEST_MESSAGE }
     end
   end
