2f8786dd1c1f0b65a6430891df3b1b18ee322f81
Josef Reidinger <jreidinger@suse.cz>
2015-01-22 15:54:59 +0100

    improve usability of terms in new code

--- src/ruby/yast/term.rb
+++ src/ruby/yast/term.rb
@@ -1,11 +1,19 @@
+require "forwardable"
+
 require 'yast/yast'
 require 'yast/builtins'
 require 'yast/ops'
 
 module Yast
-  # Represents YCP type term
+  # Represents YCP type term enhanced by some ruby convenient methods
+  #
+  # Terms can be compared and can act like array of params with mark alias value.
   class Term
     include Comparable
+    extend Forwardable
+    include Enumerable
+
+    def_delegators :@params, :each, :size, :empty?, :[], :[]=, :<<
 
     # term symbol
     attr_reader :value
@@ -17,17 +25,37 @@ module Yast
       @params = params
     end
 
-    def [] index
-      params[index]
-    end
+    # Find Object that match block even if it is in deep structure
+    # of nested terms
+    # @return [Object, nil] returns nil if doesn't find matching element
+    # @example how to find widget in complex term
+    #   # UIShortcuts included
+    #   content = VBox(
+    #               HBox(
+    #                 VBox(
+    #                   Hbox(
+    #                     InputField(Id(:input1), "Input1"),
+    #                     InputField(Id(:input2), "Input2")
+    #                   )
+    #                 )
+    #               )
+    #             )
+    #  last_hbox = content.nested_find do |t|
+    #                t.all? { |i| i.value == :InputField }
+    #              end
+    #  last_hbox << InputField(Id(:input3), "Input3") if more_info?
+    #
+    def nested_find(&block)
+      res = find(&block)
+      return res if res
 
-    def []= index, value
-      params[index] = value
-    end
+      each do |o|
+        next unless o.respond_to?(:nested_find)
+        res = o.nested_find(&block)
+        break if res
+      end
 
-    # gets number of parameters
-    def size
-      params.size
+      res
     end
 
     def clone
