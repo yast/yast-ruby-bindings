181405fef5a4f06b6f7aa07429a77cb97a88b493
Ladislav Slezák <lslezak@suse.cz>
2019-01-21 17:01:53 +0100

    Support for FastGettext 2.0 (bsc#1121865)
    (backward compatible with FastGettext 1.6)

---
+++ src/ruby/yast/translation.rb
@@ -0,0 +1,9 @@
+require "fast_gettext"
+
+module Yast
+  # Just a wrapper around FastGettext::Translation, we cannot include it
+  # directly because we define our own _() and n_() methods.
+  module Translation
+    extend FastGettext::Translation
+  end
+end
