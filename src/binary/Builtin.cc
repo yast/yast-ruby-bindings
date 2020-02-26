e74dde285e4930d230fd10deb8d695f7c02b0713
Björn Esser <besser82@fedoraproject.org>
2020-02-26 20:45:57 +0100

    Fixes for gensalt handling with libxcrypt.

--- src/binary/Builtin.cc
+++ src/binary/Builtin.cc
@@ -223,8 +223,14 @@ extern "C" {
   static char*
   make_crypt_salt (const char* crypt_prefix, int crypt_rounds)
   {
+#ifndef CRYPT_GENSALT_OUTPUT_SIZE
 #define CRYPT_GENSALT_OUTPUT_SIZE (7 + 22 + 1)
+#endif
 
+#ifdef CRYPT_GENSALT_IMPLEMENTS_AUTO_ENTROPY
+    const char *entropy = NULL;
+    const size_t entropy_len = 0;
+#else
 #ifndef RANDOM_DEVICE
 #define RANDOM_DEVICE "/dev/urandom"
 #endif
@@ -238,7 +244,8 @@ extern "C" {
     }
 
     char entropy[16];
-    if (read_loop (fd, entropy, sizeof(entropy)) != sizeof(entropy))
+    const size_t entropy_len = sizeof(entropy);
+    if (read_loop (fd, entropy, entropy_len) != entropy_len)
     {
       close (fd);
       y2error ("Unable to obtain entropy from %s\n", RANDOM_DEVICE);
@@ -246,12 +253,15 @@ extern "C" {
     }
 
     close (fd);
+#endif
 
     char output[CRYPT_GENSALT_OUTPUT_SIZE];
     char* retval = crypt_gensalt_rn (crypt_prefix, crypt_rounds, entropy,
-      sizeof(entropy), output, sizeof(output));
+      entropy_len, output, sizeof(output));
 
-    memset (entropy, 0, sizeof (entropy));
+#ifndef CRYPT_GENSALT_IMPLEMENTS_AUTO_ENTROPY
+    memset (entropy, 0, entropy_len);
+#endif
 
     if (!retval)
     {
@@ -261,6 +271,7 @@ extern "C" {
 
     return strdup (retval);
   }
+
 
   // the return value should be free'd
   char *
