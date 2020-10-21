89b2eaf4673d2eda3aabba8b5362612ae5b6cab5
Martin Vidner <mvidner@suse.com>
2020-10-21 17:25:06 +0200

    Verify that changing a value in a sorted table works correctly. (RSpec)
    See https://bugzilla.opensuse.org/show_bug.cgi?id=1165388

---
+++ tests/libyui/table_sort.rb
@@ -0,0 +1,51 @@
+#! /usr/bin/env ruby
+
+# See table_sort_wrong_cell_1165388.test
+
+require_relative "../test_helper"
+require "yast"
+
+if Yast.ui_component == ""
+  Yast.ui_component = ARGV[0] || "ncurses"
+end
+
+module Yast
+  class TableCellClient < Client
+    def main
+      Yast.import "UI"
+
+      # notice that neither the ids nor the values are sorted here
+      contents = [
+	Item(Id("id-zzz-1"), "name-bbb", "value-bbb"),
+        Item(Id("id-yyy-2"), "name-ccc", "value-ccc"),
+        Item(Id("id-xxx-3"), "name-aaa", "value-aaa"),
+      ]
+      keep_sorting = WFM.Args()[0] == "no-sort"
+      opts = keep_sorting ? Opt(:keepSorting, :notify) : Opt(:notify)
+      UI.OpenDialog(
+        VBox(
+          Label("Table sorting test"),
+          MinSize(
+            25, 8,
+            Table(Id(:table), opts, Header("Name", "Value"), contents)
+          ),
+          Label("Enter/Double-click any item to uppercase the value"),
+          PushButton(Id(:cancel), "&Close")
+        )
+      )
+
+      while UI.UserInput != :cancel
+        current_item_id = UI.QueryWidget(Id(:table), :CurrentItem)
+        value = UI.QueryWidget(:table, Cell(current_item_id, 1))
+        UI.ChangeWidget(Id(:table), Cell(current_item_id, 1), value.upcase)
+      end
+      items = UI.QueryWidget(:table, :Items)
+      Builtins.y2milestone("Items: %1", items)
+
+      UI.CloseDialog
+      nil
+    end
+  end
+end
+
+Yast::TableCellClient.new.main
---
+++ tests/libyui/table_sort_spec.rb
@@ -0,0 +1,29 @@
+require_relative "rspec_tmux_tui"
+
+describe "Table" do
+  context "when it sorts the items," do
+    around(:each) do |ex|
+      yast_ncurses = "#{__dir__}/yast_ncurses"
+      @base = "table_sort"
+      @tui = TmuxTui.new_session "#{yast_ncurses} #{__dir__}/#{@base}.rb"
+      ex.run
+      @tui.kill_session if @tui.has_session?
+    end
+
+    bug = "1165388"
+    it "ChangeWidget(_, Cell(row, col)) changes the correct cell, boo##{bug}" do
+      @tui.await(/Table sorting test/)
+      @tui.capture_pane_to("#{@base}-1-initial")
+
+      @tui.send_keys "Home"     # go to first table row
+      @tui.capture_pane_to("#{@base}-2-first-row-selected")
+
+      @tui.send_keys "Enter"    # activate first table row
+      @tui.capture_pane_to("#{@base}-3-first-row-activated")
+
+      expect(@tui.capture_pane).to match(/name-aaa.VALUE-AAA/)
+
+      @tui.send_keys "M-C"      # &Close
+    end
+  end
+end
