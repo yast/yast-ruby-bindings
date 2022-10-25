df4390875bbaed6c2cc8d4a47b5ff9e7d69bab06
Ladislav Slezák <lslezak@suse.cz>
2022-10-25 17:18:54 +0200

    Added "log.group" method, allow installation via "yupdate" (#287)
    * Added "log.group" method, allow installation via "yupdate"
    
    - 4.5.4
    
    * Improved YARD comments
    
    * Update src/ruby/yast/y2logger.rb
    
    Co-authored-by: Martin Vidner <mvidner@suse.cz>
    
    * Update src/ruby/yast/y2logger.rb
    
    Co-authored-by: Martin Vidner <mvidner@suse.cz>
    
    * Code review fixes
    
    * Make error values configurable
    
    * Added unit test
    
    * Update src/ruby/yast/y2logger.rb
    
    Co-authored-by: Martin Vidner <mvidner@suse.cz>
    
    * Version 4.5.4
    
    * Update documentation
    
    * Update src/ruby/yast/y2logger.rb
    
    Co-authored-by: Martin Vidner <mvidner@suse.cz>
    
    Co-authored-by: Martin Vidner <mvidner@suse.cz>

--- src/ruby/yast/y2logger.rb
+++ src/ruby/yast/y2logger.rb
@@ -6,6 +6,44 @@ require "singleton"
 require "yast/logger"
 
 module Yast
+
+  # Stores the log group result data, used in {Y2Logger.group}.
+  class LogGroupResult
+    # @return [String,nil] result of the step as a textual description
+    attr_accessor :summary
+
+    # @return [Object] result of the block
+    attr_accessor :result
+
+    # @param value [Boolean] set to false if the result of the group is a failure,
+    #  overrides the state evaluated from the the {#result} attribute
+    attr_writer :success
+
+    # @return [Array] list of error values (*result* values that will make *success* false)
+    attr_accessor :error_values
+
+    # was the execution of the block successful?
+    # @return [Boolean] true if the block succeeded, false otherwise
+    def success?
+      if success.nil?
+        !error_values.include?(result)
+      else
+        success
+      end
+    end
+
+    def initialize
+      # these return values are considered failures by default,
+      # can be overridden by modifying the `error_values` list
+      @error_values = [:abort, :cancel, false]
+    end
+
+  private
+
+    # @return [Boolean,nil] explicit failure
+    attr_reader :success
+  end
+
   # A Ruby Logger which wraps Yast.y2*() calls
   class Y2Logger < ::Logger
     include Singleton
@@ -40,6 +78,41 @@ module Yast
       # process also debug messages but might not be logged in the end
       self.level = ::Logger::DEBUG
     end
+
+    # log a block of commands, adds a special begin and end markers into the log,
+    # the block should be one big logical step in the process,
+    # can be used recursively, e.g. log.group might call another log.group inside
+    # @param description [String] short description of the block
+    # @param block [Proc] block to call
+    # @yieldparam group [LogGroupResult] can be optionally used to pass result details
+    # @yieldreturn [Object] passed on;
+    #   if one of `false`, `:abort`, `:cancel` ({LogGroupResult#error_values}),
+    #   the group is logged as failed (log.error)
+    # @return [Object] whatever the *block* returned
+    def group(description, &block)
+      details = LogGroupResult.new
+      # mark start of the group
+      info "::group::#{Process.clock_gettime(Process::CLOCK_MONOTONIC)}::#{description}"
+
+      if block_given?
+        ret = block.call(details)
+      else
+        raise ArgumentError, "Missing a block"
+      end
+      details.result = ret
+
+      ret
+    rescue StandardError => e
+      # mark a failure
+      details.success = false
+      details.summary = "Raised exception: #{e}"
+      # reraise the original exception
+      raise
+    ensure
+      # mark end of the group with result data, if it failed log as an error
+      level = details.success? ? :info : :error
+      public_send(level, "::endgroup::#{Process.clock_gettime(Process::CLOCK_MONOTONIC)}::#{details.summary}")
+    end
   end
 
   # This module provides access to Yast specific logging
@@ -60,6 +133,23 @@ module Yast
   #
   #          # Builtins.y2error() replacement
   #          log.error "error"
+  #
+  #          # log a logical group of commands, useful for big tasks which
+  #          # log too many details
+  #          log.group("Adding repositories") do
+  #            add_repositories
+  #          end
+  #
+  #          # log a logical group of commands with result details
+  #          log.group("Adding repositories") do |group|
+  #            ret, repos = add_repositories
+  #            if ret == :failed
+  #              group.summary = "Could not add repositories"
+  #              group.success = false
+  #            else
+  #              group.summary = "Added #{repos.size} repositories"
+  #            end
+  #          end
   #        end
   #      end
   #    end
