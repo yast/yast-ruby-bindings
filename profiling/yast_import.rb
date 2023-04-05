baee79f6c3555e3613d85158f658cc89d5540046
Josef Reidinger <jreidinger@suse.cz>
2023-04-05 12:36:42 +0200

    Apply suggestions from code review
    Co-authored-by: Martin Vidner <mvidner@suse.cz>

--- profiling/yast_import.rb
+++ profiling/yast_import.rb
@@ -1,5 +1,13 @@
 require "memory_profiler"
 
+usage = <<CMD
+cd build # cmake ..; make
+# use the built version
+ruby -r ../tests/test_helper.rb ../profiling/yast_import.rb  # | head -n2
+# use the system version
+ruby                            ../profiling/yast_import.rb  # | head -n2
+CMD
+
 MemoryProfiler.report {
   require "yast"
 
